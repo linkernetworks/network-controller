2cae38d1d7bdbe971e94d0b1b240bce0444c400b
John-Lin <linton.tw@gmail.com>
2018-05-31 01:23:09 +0800

    add bridge

--- ovs/ovs.go
+++ ovs/ovs.go
@@ -6,6 +6,14 @@ import (
 	"github.com/digitalocean/go-openvswitch/ovs"
 )
 
+// ovs-vsctl --may-exist add-br ovsbr0
+func AddBridge(bridgeName string) error {
+	if err := c.VSwitch.AddBridge(bridgeName); err != nil {
+		return fmt.Errorf("failed to add bridge: %v", err)
+	}
+	return nil
+}
+
 // ovs-vsctl add-port br0 eth0
 func AddPort(bridgeName, ifName string) error {
 	c := ovs.New(ovs.Sudo())
