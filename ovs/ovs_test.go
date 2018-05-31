fa3de2b3cbff5d9f17115b83c50791f244483c15
hwchiu <hwchiu@linkernetworks.com>
2018-05-31 09:57:44 +0000

    Add testing

--- ovs/ovs_test.go
+++ ovs/ovs_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/digitalocean/go-openvswitch/ovs"
 	"github.com/stretchr/testify/assert"
 	"os"
+	"os/exec"
 	"testing"
 )
 
@@ -17,4 +18,53 @@ func TestAddBridge(t *testing.T) {
 	assert.NoError(t, err)
 	c := ovs.New(ovs.Sudo())
 	defer c.VSwitch.DeleteBridge(bridgeName)
+}
+
+func TestAddBridgeFail(t *testing.T) {
+	if _, ok := os.LookupEnv("TEST_OVS"); !ok {
+		t.SkipNow()
+	}
+
+	ovsFile := "/usr/bin/ovs-vsctl"
+	info, err := os.Stat(ovsFile)
+	assert.NoError(t, err)
+
+	err = os.Chmod(ovsFile, 0666)
+	defer os.Chmod(ovsFile, info.Mode())
+	assert.NoError(t, err)
+
+	bridgeName := "bridge0"
+	err = AddBridge(bridgeName)
+	assert.Error(t, err)
+	c := ovs.New(ovs.Sudo())
+	defer c.VSwitch.DeleteBridge(bridgeName)
+}
+
+func TestAddDelPort(t *testing.T) {
+	if _, ok := os.LookupEnv("TEST_OVS"); !ok {
+		t.SkipNow()
+	}
+
+	bridgeName := "bridge0"
+	err := AddBridge(bridgeName)
+	c := ovs.New(ovs.Sudo())
+	defer c.VSwitch.DeleteBridge(bridgeName)
+
+	linkName := "test0"
+	err = exec.Command("ip", "link", "add", linkName, "type", "veth", "peer", "name", linkName+"_peer").Run()
+	assert.NoError(t, err)
+	defer exec.Command("ip", "link", "del", linkName).Output()
+	err = AddPort(bridgeName, linkName)
+	assert.NoError(t, err)
+
+	br, err := c.VSwitch.PortToBridge(linkName)
+	assert.NoError(t, err)
+	assert.Equal(t, br, bridgeName)
+
+	err = DeletePort(bridgeName, linkName)
+	assert.NoError(t, err)
+
+	_, err = c.VSwitch.PortToBridge(linkName)
+	assert.Error(t, err)
+
 }
