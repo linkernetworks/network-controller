ffd656598a9813fd6498645f937ef6334089f7ed
hwchiu <hwchiu@linkernetworks.com>
2018-05-31 11:55:10 +0800

    The simple gotest for ovs function

---
+++ ovs/ovs_test.go
@@ -0,0 +1,20 @@
+package ovs
+
+import (
+	"github.com/digitalocean/go-openvswitch/ovs"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+func TestAddBridge(t *testing.T) {
+	if _, ok := os.LookupEnv("TEST_OVS"); !ok {
+		t.SkipNow()
+	}
+
+	bridgeName := "bridge0"
+	err := AddBridge(bridgeName)
+	c := ovs.New(ovs.Sudo())
+	defer c.VSwitch.DeleteBridge(bridgeName)
+	assert.NoError(t, err)
+}
