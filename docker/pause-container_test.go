57d1717790bf07907ca7e670631bc62e5c517ef5
hwchiu <hwchiu@linkernetworks.com>
2018-06-02 17:07:17 +0000

    Add invalid testing for pauuse-container

--- docker/pause-container_test.go
+++ docker/pause-container_test.go
@@ -22,3 +22,24 @@ func TestFindK8SPauseContainerID(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, "18720aa37e71", cid, "they should be equal")
 }
+
+func TestFindK8SPauseContainerIDFail(t *testing.T) {
+	containers := []types.Container{
+		types.Container{
+			ID:    "18720aa37e71",
+			Names: []string{"k8s_POD_mongo-0_default_553be55e-532c-11e8-bea4-42010af0009f_0"},
+		},
+		types.Container{
+			ID:    "12332aa37f99",
+			Names: []string{"k8s_POD_redis_default_667be33e-178c-99e2-bea6-68090bd0001d_20"},
+		},
+	}
+	//Invalid Regexp Pattern
+	cid, err := FindK8SPauseContainerID(containers, "(+", "default", "553be55e-532c-11e8-bea4-42010af0009f")
+	assert.Error(t, err)
+	assert.Equal(t, "", cid)
+
+	cid, err = FindK8SPauseContainerID(containers, "no-exist", "default", "553be55e-532c-11e8-bea4-42010af0009f")
+	assert.NoError(t, err)
+	assert.Equal(t, "", cid)
+}
