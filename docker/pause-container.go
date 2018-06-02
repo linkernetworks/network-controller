025c0b05fb829bfbe69dbfdb3a187e821558354e
John-Lin <linton.tw@gmail.com>
2018-06-02 23:01:59 +0800

    error handling

--- docker/pause-container.go
+++ docker/pause-container.go
@@ -11,10 +11,10 @@ func FindK8SPauseContainerID(containers []types.Container, PodName, Namespace, P
 	// ex: k8s_POD_myinit_default_05ab36d8-65aa-11e8-b35e-42010af00248_0
 	pattern := fmt.Sprintf("k8s_POD_%s_%s_%s_\\d+", PodName, Namespace, PodUUID)
 	r, err := regexp.Compile(pattern)
+	if err != nil {
+		return "", err
+	}
 	for _, container := range containers {
-		if err != nil {
-			return "", err
-		}
 		// the first container name would be fine
 		// don't know why container will have more than one name
 		if r.MatchString(container.Names[0]) {
