bd12af1dace217dbad48919b79ed86b67311e0e5
hwchiu <hwchiu@linkernetworks.com>
2018-06-08 03:57:59 +0000

    Prototype of netlink event handler

---
+++ netlink/netlink_event.go
@@ -0,0 +1,65 @@
+package netlinkEvent
+
+import (
+	"github.com/vishvananda/netlink"
+	"golang.org/x/sys/unix"
+	"log"
+)
+
+type NlEventHandler struct {
+	stop              chan struct{}
+	LinkDeleteHandler []LinkReceiver
+}
+
+type LinkReceiver func(lu netlink.LinkUpdate) bool
+
+func New() *NlEventHandler {
+	return &NlEventHandler{
+		stop: make(chan struct{}),
+	}
+}
+
+func (nl *NlEventHandler) AddDeletedLinkHandler(handler LinkReceiver) {
+	nl.LinkDeleteHandler = append(nl.LinkDeleteHandler, handler)
+}
+
+func (nl *NlEventHandler) TrackNetlink() error {
+
+	stop := make(chan struct{})
+	data := make(chan netlink.LinkUpdate)
+	if err := netlink.LinkSubscribe(data, stop); err != nil {
+		return err
+	}
+
+	for {
+		select {
+		case c := <-data:
+			log.Println("Type", c.Header.Type)
+			switch c.Header.Type {
+			case unix.RTM_DELLINK:
+				for _, v := range nl.LinkDeleteHandler {
+					if v(c) == true {
+						nl.Stop()
+					}
+				}
+
+			}
+
+		case <-nl.stop:
+			log.Println("Receive Stop")
+			var e struct{}
+			stop <- e
+			return nil
+		}
+	}
+}
+
+func (nl *NlEventHandler) Stop() {
+	if nl.stop == nil {
+		return
+	}
+	log.Println("Stop the netlink event tracker")
+	var e struct{}
+	nl.stop <- e
+	close(nl.stop)
+}
---
+++ netlink/netlink_event_test.go
@@ -0,0 +1,32 @@
+package netlinkEvent
+
+import (
+	"os/exec"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/vishvananda/netlink"
+)
+
+func TestNetlinkEventTrack(t *testing.T) {
+	wait := make(chan struct{})
+	tracker := New()
+	var handler = func(lu netlink.LinkUpdate) bool {
+		var e struct{}
+		wait <- e
+		return false
+	}
+
+	tracker.AddDeletedLinkHandler(handler)
+
+	hName := "test0"
+	cName := "test0_peer"
+	err := exec.Command("ip", "link", "add", hName, "type", "veth", "peer", "name", cName).Run()
+	assert.NoError(t, err)
+
+	go tracker.TrackNetlink()
+
+	exec.Command("ip", "link", "del", hName).Output()
+	<-wait
+	tracker.Stop()
+}
